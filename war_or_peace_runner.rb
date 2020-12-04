ecfed9d9f6cdb42265f96c00c6d4bc094bee704f
Andrew Johnston <av.johnst@gmail.com>
2020-12-04 13:53:23 -0700

    Create war_or_peace_runner.rb and add card variables

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,59 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+
+
+card1 = :heart, '2', 2
+card2 = :heart, '3', 3
+card3 = :heart, '4', 4
+card4 = :heart, '5', 5
+card5 = :heart, '6', 6
+card6 = :heart, '7', 7
+card7 = :heart, '8', 8
+card8 = :heart, '9', 9
+card9 = :heart, '10', 10
+card10 = :heart, 'Jack', 11
+card11 = :heart, 'Queen', 12
+card12 = :heart, 'King', 13
+card13 = :heart, 'Ace', 14
+card14 = :club, '2', 2
+card15 = :club, '3', 3
+card16 = :club, '4', 4
+card17 = :club, '5', 5
+card18 = :club, '6', 6
+card19 = :club, '7', 7
+card20 = :club, '8', 8
+card21 = :club, '9', 9
+card22 = :club, '10', 10
+card23 = :club, 'Jack', 11
+card24 = :club, 'Queen', 12
+card25 = :club, 'King', 13
+card26 = :club, 'Ace', 14
+card27 = :spade, '2', 2
+card28 = :spade, '3', 3
+card29 = :spade, '4', 4
+card30 = :spade, '5', 5
+card31 = :spade, '6', 6
+card32 = :spade, '7', 7
+card33 = :spade, '8', 8
+card34 = :spade, '9', 9
+card35 = :spade, '10', 10
+card36 = :spade, 'Jack', 11
+card37 = :spade, 'Queen', 12
+card38 = :spade, 'King', 13
+card39 = :spade, 'Ace', 14
+card40 = :diamond, '2', 2
+card41 = :diamond, '3', 3
+card42 = :diamond, '4', 4
+card43 = :diamond, '5', 5
+card44 = :diamond, '6', 6
+card45 = :diamond, '7', 7
+card46 = :diamond, '8', 8
+card47 = :diamond, '9', 9
+card48 = :diamond, '10', 10
+card49 = :diamond, 'Jack', 11
+card50 = :diamond, 'Queen', 12
+card51 = :diamond, 'King', 13
+card52 = :diamond, 'Ace', 14
