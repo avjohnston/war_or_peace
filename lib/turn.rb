719e02a7a45cd2e611b04f09f72becb1ad61d053
Andrew Johnston <av.johnst@gmail.com>
2020-12-04 12:25:58 -0700

    Fix test for award_spoils_war

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,12 +43,16 @@ class Turn
       @spoils_of_war << player2.deck.remove_card
 
     elsif type == :war
-        @spoils_of_war << player1.deck.remove_card[0..2]
-        @spoils_of_war << player2.deck.remove_card[0..2]
+      3.times do
+        @spoils_of_war << player1.deck.remove_card
+        @spoils_of_war << player2.deck.remove_card
+      end
 
     else
-        player1.deck.remove_card[0..2]
-        player2.deck.remove_card[0..2]
+      3.times do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
     end
   end
 
@@ -60,3 +64,20 @@ class Turn
   end
 
 end
+
+
+# card1 = Card.new(:heart, 'Jack', 11)
+# card2 = Card.new(:heart, '10', 10)
+# card3 = Card.new(:heart, '9', 9)
+# card4 = Card.new(:diamond, 'Jack', 11)
+# card5 = Card.new(:heart, '8', 8)
+# card6 = Card.new(:diamond, 'Queen', 12)
+# card7 = Card.new(:heart, '3', 3)
+# card8 = Card.new(:diamond, '2', 2)
+# deck1 = Deck.new([card1, card2, card5, card8])
+# deck2 = Deck.new([card4, card3, card6, card7])
+# player1 = Player.new('Megan', deck1)
+# player2 = Player.new('Aurora', deck2)
+# turn = Turn.new(player1, player2)
+# winner = turn.winner
+# require'pry';binding.pry
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -141,7 +141,7 @@ class TurnTest < MiniTest::Test
   end
 
   def test_pile_cards_war
-    skip
+    # skip
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -156,11 +156,12 @@ class TurnTest < MiniTest::Test
     player2 = Player.new('Aurora', deck2)
     turn = Turn.new(player1, player2)
     winner = turn.winner
+    turn.pile_cards
 
-    assert_equal [card2, card1, card5, card4, card3, card6], turn.pile_cards
+
+    assert_equal [card1, card4, card2, card3, card5, card6], turn.spoils_of_war
 
   end
-
 
   def test_award_spoils_basic
     # skip
@@ -188,4 +189,25 @@ class TurnTest < MiniTest::Test
 
   end
 
+  def test_award_spoils_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal [card8], player1.deck.cards
+    assert_equal [card7, card1, card4, card2, card3, card5, card6], player2.deck.cards
+  end
 end
