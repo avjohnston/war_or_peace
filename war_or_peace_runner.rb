76c83a9400034de2042a5cceac2c991fcd9d4a36
Andrew Johnston <av.johnst@gmail.com>
2020-12-06 15:54:40 -0700

    Double check war_or_peace_runner.rb for bugs

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,69 +2,74 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game'
 
 
 
-card1 = :heart, '2', 2
-card2 = :heart, '3', 3
-card3 = :heart, '4', 4
-card4 = :heart, '5', 5
-card5 = :heart, '6', 6
-card6 = :heart, '7', 7
-card7 = :heart, '8', 8
-card8 = :heart, '9', 9
-card9 = :heart, '10', 10
-card10 = :heart, 'Jack', 11
-card11 = :heart, 'Queen', 12
-card12 = :heart, 'King', 13
-card13 = :heart, 'Ace', 14
-card14 = :club, '2', 2
-card15 = :club, '3', 3
-card16 = :club, '4', 4
-card17 = :club, '5', 5
-card18 = :club, '6', 6
-card19 = :club, '7', 7
-card20 = :club, '8', 8
-card21 = :club, '9', 9
-card22 = :club, '10', 10
-card23 = :club, 'Jack', 11
-card24 = :club, 'Queen', 12
-card25 = :club, 'King', 13
-card26 = :club, 'Ace', 14
-card27 = :spade, '2', 2
-card28 = :spade, '3', 3
-card29 = :spade, '4', 4
-card30 = :spade, '5', 5
-card31 = :spade, '6', 6
-card32 = :spade, '7', 7
-card33 = :spade, '8', 8
-card34 = :spade, '9', 9
-card35 = :spade, '10', 10
-card36 = :spade, 'Jack', 11
-card37 = :spade, 'Queen', 12
-card38 = :spade, 'King', 13
-card39 = :spade, 'Ace', 14
-card40 = :diamond, '2', 2
-card41 = :diamond, '3', 3
-card42 = :diamond, '4', 4
-card43 = :diamond, '5', 5
-card44 = :diamond, '6', 6
-card45 = :diamond, '7', 7
-card46 = :diamond, '8', 8
-card47 = :diamond, '9', 9
-card48 = :diamond, '10', 10
-card49 = :diamond, 'Jack', 11
-card50 = :diamond, 'Queen', 12
-card51 = :diamond, 'King', 13
-card52 = :diamond, 'Ace', 14
+card1 = Card.new(:heart, '2', 2)
+card2 = Card.new(:heart, '3', 3)
+card3 = Card.new(:heart, '4', 4)
+card4 = Card.new(:heart, '5', 5)
+card5 = Card.new(:heart, '6', 6)
+card6 = Card.new(:heart, '7', 7)
+card7 = Card.new(:heart, '8', 8)
+card8 = Card.new(:heart, '9', 9)
+card9 = Card.new(:heart, '10', 10)
+card10 = Card.new(:heart, 'Jack', 11)
+card11 = Card.new(:heart, 'Queen', 12)
+card12 = Card.new(:heart, 'King', 13)
+card13 = Card.new(:heart, 'Ace', 14)
+card14 = Card.new(:club, '2', 2)
+card15 = Card.new(:club, '3', 3)
+card16 = Card.new(:club, '4', 4)
+card17 = Card.new(:club, '5', 5)
+card18 = Card.new(:club, '6', 6)
+card19 = Card.new(:club, '7', 7)
+card20 = Card.new(:club, '8', 8)
+card21 = Card.new(:club, '9', 9)
+card22 = Card.new(:club, '10', 10)
+card23 = Card.new(:club, 'Jack', 11)
+card24 = Card.new(:club, 'Queen', 12)
+card25 = Card.new(:club, 'King', 13)
+card26 = Card.new(:club, 'Ace', 14)
+card27 = Card.new(:spade, '2', 2)
+card28 = Card.new(:spade, '3', 3)
+card29 = Card.new(:spade, '4', 4)
+card30 = Card.new(:spade, '5', 5)
+card31 = Card.new(:spade, '6', 6)
+card32 = Card.new(:spade, '7', 7)
+card33 = Card.new(:spade, '8', 8)
+card34 = Card.new(:spade, '9', 9)
+card35 = Card.new(:spade, '10', 10)
+card36 = Card.new(:spade, 'Jack', 11)
+card37 = Card.new(:spade, 'Queen', 12)
+card38 = Card.new(:spade, 'King', 13)
+card39 = Card.new(:spade, 'Ace', 14)
+card40 = Card.new(:diamond, '2', 2)
+card41 = Card.new(:diamond, '3', 3)
+card42 = Card.new(:diamond, '4', 4)
+card43 = Card.new(:diamond, '5', 5)
+card44 = Card.new(:diamond, '6', 6)
+card45 = Card.new(:diamond, '7', 7)
+card46 = Card.new(:diamond, '8', 8)
+card47 = Card.new(:diamond, '9', 9)
+card48 = Card.new(:diamond, '10', 10)
+card49 = Card.new(:diamond, 'Jack', 11)
+card50 = Card.new(:diamond, 'Queen', 12)
+card51 = Card.new(:diamond, 'King', 13)
+card52 = Card.new(:diamond, 'Ace', 14)
 
-deck1 = [card2, card4, card6, card9, card10, card12, card15, card16, card19,
-card20, card22, card24, card25, card27, card29, card30, card31, card33,
-card35, card37, card39, card42, card45, card46, card49, card52].shuffle!
 
-deck2 = [card2, card4, card6, card9, card10, card12, card15, card16, card19,
-card20, card22, card24, card25, card27, card29, card30, card31, card33,
-card35, card37, card39, card42, card45, card46, card49, card52].shuffle!
+deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13,
+card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26,
+card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39,
+card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52].shuffle!
+
+deck1 = deck[0..25].shuffle!
+deck2 = deck[26..51].shuffle!
+
+
+
 
 deck_1 = Deck.new(deck1)
 deck_2 = Deck.new(deck2)
@@ -72,31 +77,6 @@ deck_2 = Deck.new(deck2)
 player1 = Player.new('Megan', deck_1)
 player2 = Player.new('Aurora', deck_2)
 
-
-class StartGame
-  def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2
-  end
-
-  def start
-    puts "Welcome to War! (or Peace) This game will be played with 52 cards."
-    puts "The players today Megan and Aurora."
-    puts "Type 'GO' to start the game."
-
-    answer = gets.chomp
-
-    if
-      answer = "GO" || "Go" || "go"
-      puts "Now starting game..."
-    else
-      puts "Goodbye"
-    end
-
-  end
-
-
-end
-
-game = StartGame.new(player1, player2)
+game = Game.new(player1, player2)
 game.start
+game.run_game
